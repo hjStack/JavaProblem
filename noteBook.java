83b30f12756c2d052b8987ba004ec8a63f9bf819
HJ <165659365+hjStack@users.noreply.github.com>
2024-08-26 16:56:55 +0900

    o(n2)문제 해결하기

--- noteBook.java
+++ noteBook.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class noteBook {
+package PACKAGE_NAME;
+
+//알고리즘 o(n2) 해결하기 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class first_last {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String s=br.readLine();
+        int num=Integer.parseInt(s);
+
+        String s1[]=new String[10];
+
+        for(int i=0; i<num; i++){
+            s1[i]=br.readLine();
+        }
+        for(int i=0; i<num; i++){
+            System.out.print(s1[i].charAt(0));
+            int lastIndex=s1[i].length()-1;
+            System.out.print(s1[i].charAt(lastIndex));
+            System.out.println();
+        }
+
+    }
 }
