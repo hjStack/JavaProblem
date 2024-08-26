691ce2ab39a316bdd994e78c8afdfddfc58b2305
HJ <165659365+hjStack@users.noreply.github.com>
2024-08-26 16:55:43 +0900

    Update first_last.java

--- first_last.java
+++ first_last.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class first_last {
+
+//첫글자와 마지막 문자 출력하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class first_last {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String s=br.readLine();
+        int num=Integer.parseInt(s);
+
+        String s1[]=new String[10];
+
+        for(int i=0; i<num; i++){
+            s1[i]=br.readLine();
+        }
+        for(int i=0; i<num; i++){
+            System.out.print(s1[i].charAt(0));
+            int lastIndex=s1[i].length()-1;
+            System.out.print(s1[i].charAt(lastIndex));
+            System.out.println();
+        }
+
+    }
 }
